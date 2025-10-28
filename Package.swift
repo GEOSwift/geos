65a12e7ccd5fcbde5ed8eb8a92ea7c178b77a4d9
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-28 11:33:59 -0700

    Update Package.swift to successfull built the geos dynamic library.

--- Package.swift
+++ Package.swift
@@ -1,14 +1,16 @@
-// swift-tools-version: 6.2
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "geos",
+    platforms: [.iOS(.v12), .macOS(.v10_13), .tvOS(.v12), .watchOS(.v4), .visionOS(.v1)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "geos",
+            type: .dynamic,
             targets: ["geos"]
         ),
     ],
@@ -16,7 +18,15 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "geos"
+            name: "geos",
+            publicHeadersPath: "public",
+            cxxSettings: [
+                .define("USE_UNSTABLE_GEOS_CPP_API"),
+                .define("NDEBUG"),
+                .headerSearchPath("include"),
+                .headerSearchPath("src/deps")
+            ]
         )
-    ]
+    ],
+    cxxLanguageStandard: .cxx17
 )
