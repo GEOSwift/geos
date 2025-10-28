520a892a99699a8743f6f585a12b294e357335c7
Scott Hoyt <scottrhoyt@gmail.com>
2025-10-28 11:26:04 -0700

    Remove Tests.

--- Package.swift
+++ Package.swift
@@ -17,10 +17,6 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "geos"
-        ),
-        .testTarget(
-            name: "geosTests",
-            dependencies: ["geos"]
-        ),
+        )
     ]
 )
